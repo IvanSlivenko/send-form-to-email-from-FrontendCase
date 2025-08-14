d44881a8e8b6cffd9cc6a40a6e82e14aad483ea4
IvanSlivenko <ivan8822@ukr.net>
2025-08-14 18:07:52 +0300

    14-08-2025-18-07

--- start-template-mail/client/js/index.js
+++ start-template-mail/client/js/index.js
@@ -22,9 +22,25 @@ async function submitForm(event) {
 
     const validationErrors = validateForm(formDataObject);
 
+    displayErrors(validationErrors);
+        if(validationErrors.length > 0) return;
+
     
 
     console.log(`Було натиснути на кнопку ${formBtn.textContent}`)
+}
+
+function displayErrors(errors){
+    const errorElement = document.querySelectorAll('.form__error');
+    errorElement.forEach((errorElement) => errorElement.textContent='')
+
+    if(errors.length < 1) return;
+
+    errors.forEach((error)=>{
+        const {field, message} = error;
+        const errorElement = document.querySelector(`[data-for="${field}"]`);
+        errorElement.textContent = message;
+    })
 }
 
 function validateForm(formData) {
@@ -36,27 +52,27 @@ function validateForm(formData) {
     const errors = [];
 
     if (!name) {
-        errors.push({ field: 'name', message: 'Пожалуйста, введите ваше ФИО.' });
+        errors.push({ field: 'name', message: 'Будь-ласка введіть ваше ПІП.' });
     } else if (name.length < 5 || name.length > 20) {
-        errors.push({ field: 'name', message: 'Пожалуйста, введите корректные данные. Пример: Быков Иван Петрович' });
+        errors.push({ field: 'name', message: 'Будь-ласка, введіть корректні відомості. Наприклад: Биков Іван Петрович' });
     }
 
     if (!phone) {
-        errors.push({ field: 'phone', message: 'Пожалуйста, введите номер телефона.' });
+        errors.push({ field: 'phone', message: 'Будь-ласка, введіть номер телефону.' });
     } else if (!phoneRegex.test(phone)) {
-        errors.push({ field: 'phone', message: 'Пожалуйста, введите корректный номер телефона. Пример: +375257851204' });
+        errors.push({ field: 'phone', message: 'Будь-ласка, введіть корректний номер телефону. Наприклад: 067 470 87 21' });
     }
 
     if (!email) {
-        errors.push({ field: 'email', message: 'Пожалуйста, введите адрес электронной почты.' });
+        errors.push({ field: 'email', message: 'Будь-ласка, введіть адресу электронної пошти.' });
     } else if (!emailRegex.test(email) || (email.length < 5 || email.length > 100)) {
-        errors.push({ field: 'email', message: 'Пожалуйста, введите корректный адрес электронной почты. Пример: frontend@gmail.com' });
+        errors.push({ field: 'email', message: 'Будь-ласка, введіть корректну адресу электронної пошти. Наприклад: frontend@gmail.com' });
     }
 
     if (!message) {
-        errors.push({ field: 'message', message: 'Пожалуйста, введите сообщение.' });
+        errors.push({ field: 'message', message: 'Будь-ласка введіть повідомлення.' });
     } else if (message.length < 20 || message.length > 400) {
-        errors.push({ field: 'message', message: 'В сообщении должно быть мин. 20 и не более 400 символов.' });
+        errors.push({ field: 'message', message: 'В повідомленні має бути мін. 20 і не більше 400 символів.' });
     }
     
     return errors;
